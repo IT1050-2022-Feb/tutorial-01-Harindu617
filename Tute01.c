2d26b1da21297a345d9797f501758d7e9b2d4f3c
Harindu Abhayathunge <99375057+Harindu617@users.noreply.github.com>
2022-02-18 04:46:22 +0000

    what did you change

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  int sub1, sub2, sum = 0;
+  float avg = 0;
+
+  printf("Enter the marks of first subject : ");
+  scanf("%d", &sub1);
+
+  printf("Enter the marks of second subject : ");
+  scanf("%d", &sub2); 
+
+  sum = sub1 + sub2;
+  avg = sum / 2.0;
+
+  printf("Avarage of the two marks : %.2f", avg);
+
   return 0;
 }
 
